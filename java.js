1071836f30272b5b458aa95da09e2064f863d674
nikitham007 <nikithadm007@gmail.com>
2026-06-12 10:29:20 +0530

    variables example

---
+++ java.js
@@ -0,0 +1,11 @@
+// variables declaration using var
+var name="javascript"
+console.log(name)
+// variables declaration using let
+{
+    let a=20
+    console.log(a)
+}
+//variables declaration using const
+const value="vetias"
+console.log(value)
