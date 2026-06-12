cb72dfc70fa946411ca3294e16188ecefc12ae06
nikitham007 <nikithadm007@gmail.com>
2026-06-12 15:48:52 +0530

    function

---
+++ datatype.js
@@ -0,0 +1,40 @@
+console.log("number")
+var ra=18
+console.log(ra)
+//string
+console.log("datatype string")
+var name="roja"
+console.log(name)
+//boolean
+console.log("datatype boolean")
+var student=true
+console.log(student)
+//bigint
+console.log("bid int")
+var big=759348947
+console.log(big)
+//null
+console.log("null:")
+var emptyvalue="null"
+console.log(emptyvalue)
+//undefined
+console.log("undefined") 
+var undefinedvalue
+console.log(undefinedvalue)
+//symbol
+console.log("symbol")
+var symbol=("$")
+console.log(symbol)
+//nonprimitive
+console.log("nonprimitive")
+console.log("array")
+var a=["apple,jackfruit,mango,banana"]
+console.log(Number.length)
+console.log(number[2])
+//object
+console.log("object")
+var person={
+    name:"nikitha"
+    age=18
+}
+console.log(person.age)
---
+++ function.js
@@ -0,0 +1,14 @@
+console.log("FUNCTION")
+
+function greet()
+{
+    console.log("hello world")
+}
+greet()
+
+console.log("PARAMETIC")
+function add(a,b)
+{
+    console.log(a+b)
+}
+add(4,7)
