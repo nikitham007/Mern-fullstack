0406363b6cf4052656b215b15ab9c9658b569828
nikitham007 <nikithadm007@gmail.com>
2026-06-16 10:05:18 +0530

    favorite

---
+++ excerice1.js
@@ -0,0 +1,11 @@
+//function
+var factor="sk"
+var fplayer="M.S,Dhoni"
+var fmovie="24"
+function favorite()
+{
+    console.log("favorite actor :"+factor)
+    console.log("favorite player :"+fplayer)
+    console.log("favorite movie :"+fmovie)
+}
+favorite()
